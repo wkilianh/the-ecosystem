328b1038ed23e61afd7a5f88adbfb228d59c99be
wkilianh <kilian.hirt@gmail.com>
2021-03-02 16:00:26 +0100

    add routes, controller, views for conversations

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,6 @@
 class ProfilesController < ApplicationController
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/destroy'
-  get 'posts/test'
   devise_for :users
   root to: 'pages#home'
   get '/search', to: 'pages#index'
   post '/comments/:comment_id/replies', to: 'replies#create'
   get '/profiles/:id', to: 'profiles#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :conversations, only: [:create, :index, :show, :destroy] do
+  end
+
   resources :posts do
     resources :comments, only: [:create]
   end
