84d247b25578512d6523aeb5fb8d09c78dabce15
wkilianh <kilian.hirt@gmail.com>
2021-03-02 15:35:36 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/search', to: 'pages#index'
+  post '/comments/:comment_id/replies', to: 'replies#create'
+  get '/profiles/:id', to: 'profiles#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 end
