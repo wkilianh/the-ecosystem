5ede76717971936391754dffe7722a983c8ab940
wkilianh <kilian.hirt@gmail.com>
2021-03-02 18:46:46 +0100

    minor change to routes and seeds

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ApplicationRecord
   belongs_to :conversation
   # belongs_to :user
   # belongs_to :conversation, through: :user
-  belongs_to :user, through: :conversation
+
+  # belongs_to :user, through: :conversation
 end
