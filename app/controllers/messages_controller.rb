303c81c8e7fc3161489de46620cd19f5ef938f62
azeer esmail <azeer.esmail@gmail.com>
2021-03-03 12:41:20 +0200

    implemented chat and action cable

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+
+  def create
+    @conversation = Conversation.find(params[:conversation_id])
+    @message = Message.new(message_params)
+    @message.conversation = @conversation
+    @message.user = current_user
+    authorize @message
+    if @message.save
+      ConversationChannel.broadcast_to(
+        @conversation,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+      redirect_to conversation_path(@conversation, anchor: "message-#{@message.id}")
+    else
+      render "conversations/"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
