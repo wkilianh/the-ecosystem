2f1f9fa8a69845a83f18383c2fa3f5869b036b75
wkilianh <kilian.hirt@gmail.com>
2021-03-03 17:08:46 +0100

    add redirect in new registrations controller, fix route to this controller. Add fields to form, fix allowed params in application controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  # def after_sign_up_path_for(resource)
-  #   raise
-  #   # profile_path# '/an/example/path' # Or :prefix_to_your_route
-  # end
+  def after_sign_up_path_for(resource)
+    profile_path(current_user) # '/an/example/path' # Or :prefix_to_your_route
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  # devise_for :users
   devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   get '/search', to: 'pages#index'
   post '/comments/:comment_id/replies', to: 'replies#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # below code routes to our own registrations_controller that will redirect to profile after signup
-
   resources :profiles, only: [:show, :edit, :update]
 
   resources :conversations, only: [:create, :index, :show, :destroy]
