7c7d3ca1a5cac676bff116c8295081481e16c80a
azeer esmail <azeer.esmail@gmail.com>
2021-03-05 12:19:44 +0200

    minor fixes in conversations and posts

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -13,6 +13,19 @@ class ConversationsController < ApplicationController
     @message = Message.new
     authorize @conversation
     # authorize @messages
+    ################# for unseen notifications #################
+    # if @conversation.sender == current_user
+    #   receiver_unseen_msgs = conversation.messages.where(user: conversation.receiver, seen: false)
+    #   receiver_unseen_msgs.each do |msg|
+    #     msg.update(seen: true)
+    #   end
+    # else
+    #   sender_unseen_msgs = conversation.messages.where(user: conversation.sender, seen: false)
+    #   sender_unseen_msgs.each do |msg|
+    #     msg.update(seen: true)
+    #   end
+    # end
+
   end
 
   def destroy
